dd31bec3ed48fcbc595f9741ed16632851d5ac4c
MALKIabdessamad <83022996+MALKIabdessamad@users.noreply.github.com>
2023-01-30 22:22:45 +0100

    Add files via upload

---
+++ port scanner.py
@@ -0,0 +1,28 @@
+import socket
+
+def check_port(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        s.connect((ip, port))
+        s.close()
+        return True
+    except:
+        return False
+
+port = XPORT
+
+ranges = [
+    ('192.168.1', 256),
+    ('10.0.0', 256),
+    ('172.16', 16),
+    ('172.31', 16),
+]
+
+for prefix, size in ranges:
+    for i in range(1, size+1):
+        ip = f"{prefix}.{i}"
+        if check_port(ip, port):
+            print(f"The port {port} is open on IP {ip}")
+        else:
+            print(f"The port {port} is closed on IP {ip}")
