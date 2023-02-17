e8dc12d718c5885612b865e4a6f9f9f52e040d52
MALKIabdessamad <83022996+MALKIabdessamad@users.noreply.github.com>
2023-02-17 12:43:12 +0100

    Update port scanner.py

--- port scanner.py
+++ port scanner.py
@@ -1,30 +1,40 @@
 import socket
+import threading
 
-def check_port(ip, port):
+def check_port(ip, port, results):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(2)
         s.connect((ip, port))
         s.close()
-        return True
+        results.append((ip, port, True))
     except:
-        return False
+        results.append((ip, port, False))
 
-port = XPORT
+port = input("Enter the port number to check: ")
 
-ranges = [
-    ('192.168.1', 256),
-    ('10.0.0', 256),
-    ('172.16', 16),
-    ('172.31', 16),
-]
+ip_range = input("Enter the IP address range to check (e.g. 192.168.1, 256): ")
+prefix, size = ip_range.split(',')
+size = int(size)
 
-with open("open_ports.txt", "w") as f:
-    for prefix, size in ranges:
-        for i in range(1, size+1):
-            ip = f"{prefix}.{i}"
-            if check_port(ip, port):
-                f.write(f"{ip}:{port}\n")
-                print(f"The port {port} is open on IP {ip}")
-            else:
-                print(f"The port {port} is closed on IP {ip}")
+output_file = f"open_ports_{prefix}_{size}.txt"
+
+threads = []
+results = []
+
+for i in range(1, size+1):
+    ip = f"{prefix}.{i}"
+    t = threading.Thread(target=check_port, args=(ip, int(port), results))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+
+with open(output_file, "w") as f:
+    for ip, port, is_open in results:
+        if is_open:
+            f.write(f"{ip}:{port}\n")
+            print(f"The port {port} is open on IP {ip}")
+        else:
+            print(f"The port {port} is closed on IP {ip}")
